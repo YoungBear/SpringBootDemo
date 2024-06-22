7b0ddd25d1c6cb968e2341a185534c5abfb9aa73
YoungBear <youngbear@aliyun.com>
2024-06-22 23:41:22 +0800

    [update] 增加controller测试用例，支持mock请求头信息

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -5,13 +5,17 @@ import com.example.demo.entity.common.ResultVo;
 import com.example.demo.exception.DemoException;
 import com.example.demo.service.IEmployeeService;
 import com.example.demo.utils.ResultVoUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author youngbear
@@ -24,13 +28,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "employee", produces = MediaType.APPLICATION_JSON_VALUE)
 public class EmployeeController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
 
-    @Autowired
+    @Resource
     private IEmployeeService employeeService;
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public ResultVo<Integer> add(@RequestBody EmployeeVo employeeVo) {
+    public ResultVo<Integer> add(HttpServletRequest request, @RequestBody EmployeeVo employeeVo) {
         try {
+            String userAccount = request.getHeader("X-USER-ACCOUNT");
+            LOGGER.info("userAccount: {}", userAccount);
             return ResultVoUtils.success(employeeService.addEmployee(employeeVo));
         } catch (DemoException demoException) {
             return ResultVoUtils.error(demoException);
--- src/test/java/com/example/demo/controller/EmployeeControllerTest.java
+++ src/test/java/com/example/demo/controller/EmployeeControllerTest.java
@@ -1,14 +1,17 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.EmployeeVo;
 import com.example.demo.entity.common.Result;
+import com.example.demo.enums.ErrorEnum;
+import com.example.demo.exception.DemoException;
 import com.example.demo.service.IEmployeeService;
 import com.example.demo.utils.ResourceFileReadUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -16,6 +19,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import javax.annotation.Resource;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 /**
@@ -27,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @description test for @link EmployeeController
  */
 @WebMvcTest(EmployeeController.class)
+@SpringJUnitConfig(classes = EmployeeController.class)
 @DisplayName("EmployeeController Test")
 public class EmployeeControllerTest {
     @Resource
@@ -36,17 +42,35 @@ public class EmployeeControllerTest {
     private IEmployeeService employeeService;
 
     @Test
-    public void addTest() throws Exception {
-        Mockito.when(employeeService.addEmployee(any())).thenReturn(1);
+    @DisplayName("add 成功")
+    public void add_test_success() throws Exception {
+        when(employeeService.addEmployee(any())).thenReturn(1);
         mockMvc.perform(MockMvcRequestBuilders.post("/employee/add")
-                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-USER-ACCOUNT", "John")
                         .content(ResourceFileReadUtils.readString("json/employee/EmployeeAdd.json"))
+                        .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(jsonPath("$.code").value(Result.SUCCESS_CODE))
                 .andExpect(jsonPath("$.msg").value(Result.SUCCESS_MESSAGE))
                 .andExpect(jsonPath("$.result.total").value(1))
                 .andExpect(jsonPath("$.result.data[0]").value(1));
-        Mockito.verify(employeeService).addEmployee(any());
+        verify(employeeService).addEmployee(any());
+    }
+
+    @Test
+    @DisplayName("add 异常")
+    public void add_test_exception() throws Exception {
+        when(employeeService.addEmployee(any(EmployeeVo.class))).thenThrow(new DemoException(ErrorEnum.UNKNOWN_ERROR));
+        ErrorEnum errorEnum = ErrorEnum.UNKNOWN_ERROR;
+        mockMvc.perform(MockMvcRequestBuilders.post("/employee/add")
+                        .header("X-USER-ACCOUNT", "John")
+                        .content(ResourceFileReadUtils.readString("json/employee/EmployeeAdd.json"))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.code").value(errorEnum.getErrorCode()))
+                .andExpect(jsonPath("$.msg").value(errorEnum.getErrorMessage()));
+        verify(employeeService).addEmployee(any());
     }
 }
